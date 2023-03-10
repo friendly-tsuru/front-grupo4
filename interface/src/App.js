6f28597af886018fb42b5e04c890f7fe95c22d33
Jardejane <janefeitosalima49@gmail.com>
2023-03-10 19:16:27 -0300

    Creating the css dasboard

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {RegistrePacient} from './doctor/pages/register/index'
+import { Dashboard} from './doctor/pages/dashboard/dashboard'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path='/' element={<RegistrePacient/>}/>
+        <Route path='Dashboard' element={< Dashboard/>} />
       </Routes>
     </Router>
   );
